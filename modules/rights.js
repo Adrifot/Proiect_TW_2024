3fa1a28e400a2c96f1a6b8b4e62f2b3bb94b26f9
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-06-22 19:37:07 +0300

    Added more rights

--- modules/rights.js
+++ modules/rights.js
@@ -2,7 +2,10 @@ const Rights = {
     viewUsers: Symbol("viewUsers"),
     deleteUsers: Symbol("deleteUsers"),
     buyProducts: Symbol("buyProducts"),
-    viewGraphs: Symbol("viewGraphs")
+    viewGraphs: Symbol("viewGraphs"),
+    addProducts: Symbol("addProducts"),
+    deleteProducts: Symbol("deleteProducts"),
+    modifyProducts: Symbol("modifyProducts")
 }
 
 module.exports = Rights;
